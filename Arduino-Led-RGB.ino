0ad688baec8ed9ec657b923bf0e128476ad16e86
Jeremy (Geek-Stuff) <geek.stuff42@gmail.com>
2016-05-10 22:46:17 +0200

    Create method   powerMyLed

--- Arduino-Led-RGB.ino
+++ Arduino-Led-RGB.ino
@@ -1,26 +1,43 @@
-const int Red   = 3;
-const int Green = 5;
-const int Blue  = 6;
+const int pinRed   = 3;
+const int pinGreen = 5;
+const int pinBlue  = 6;
 
 void setup() {
-  pinMode (Red,OUTPUT);
-  pinMode (Green,OUTPUT);
-  pinMode (Blue,OUTPUT);
+  pinMode (pinRed,OUTPUT);
+  pinMode (pinGreen,OUTPUT);
+  pinMode (pinBlue,OUTPUT);
 }
 
 void loop() {
   // Start Red color
-  digitalWrite(Red,HIGH);
-  delay(1000); 
-  digitalWrite(Red,LOW);
+  powerMyLed(1,0,0);
+  delay(1000);
 
   //Start Green color
-  digitalWrite(Green,HIGH);
-  delay(1000); 
-  digitalWrite(Green,LOW);
+  powerMyLed(0,1,0);
+  delay(1000);
 
   // Start Blue color
-  digitalWrite(Blue,HIGH);
-  delay(1000); 
-  digitalWrite(Blue,LOW);
+  powerMyLed(0,0,1);
+  delay(1000);
+}
+
+void powerMyLed(int Red, int Green, int Blue) {
+ if (Red==1) {
+  digitalWrite(pinRed,HIGH);
+ } else { 
+  digitalWrite(pinRed, LOW);
+ }
+
+ if (Green==1) {
+  digitalWrite(pinGreen,HIGH);
+ } else { 
+  digitalWrite(pinGreen, LOW);
+ }
+
+ if (Blue==1) {
+  digitalWrite(pinBlue,HIGH);
+ } else { 
+  digitalWrite(pinBlue, LOW);
+ }
 }
