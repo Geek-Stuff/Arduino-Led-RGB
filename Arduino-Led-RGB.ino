a029f6b647c3cc61dd8f0fce9430f9f6d56ac5e5
Jeremy (Geek-Stuff) <geek.stuff42@gmail.com>
2016-05-10 22:37:33 +0200

    first colors

--- Arduino-Led-RGB.ino
+++ Arduino-Led-RGB.ino
@@ -1,9 +1,26 @@
-void setup() {
-  // put your setup code here, to run once:
+const int Red   = 3;
+const int Green = 5;
+const int Blue  = 6;
 
+void setup() {
+  pinMode (Red,OUTPUT);
+  pinMode (Green,OUTPUT);
+  pinMode (Blue,OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Start Red color
+  digitalWrite(Red,HIGH);
+  delay(1000); 
+  digitalWrite(Red,LOW);
 
+  //Start Green color
+  digitalWrite(Green,HIGH);
+  delay(1000); 
+  digitalWrite(Green,LOW);
+
+  // Start Blue color
+  digitalWrite(Blue,HIGH);
+  delay(1000); 
+  digitalWrite(Blue,LOW);
 }
